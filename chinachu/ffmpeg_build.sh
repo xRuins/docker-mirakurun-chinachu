c3652e623ebfe9df8f88aaf2b08f99c664edb692
Ruins <ruinscorocoro@gmail.com>
2017-02-05 17:25:06 +0900

    add ffmpeg features and autorec concerns

---
+++ chinachu/ffmpeg_build.sh
@@ -0,0 +1,109 @@
+#!/bin/sh
+
+cpu_core=8
+enable_h264=1
+enable_h265=1
+enable_vp8=1
+enable_aac=1
+enable_mp3=1
+enable_ogg=1
+enable_opus=1
+enable_ass=1
+
+src_dir="$HOME/ffmpeg_sources"
+prefix_dir="/usr/local/ffmpeg_build"
+
+export PATH=$prefix_dir/bin:$PATH
+export PKG_CONFIG_PATH="$prefix_dir/lib/pkgconfig"
+enable_option=""
+
+repo_yasm="git://github.com/yasm/yasm.git"
+repo_x264="https://git.videolan.org/git/x264.git"
+repo_x265="https://github.com/videolan/x265.git"
+repo_aac="git://github.com/mstorsjo/fdk-aac"
+repo_opus="git://github.com/xiph/opus.git"
+repo_libvpx="https://chromium.googlesource.com/webm/libvpx.git"
+repo_libass="https://github.com/libass/libass.git"
+repo_ffmpeg="git://github.com/FFmpeg/FFmpeg"
+
+url_autoconf="http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz"
+url_lame="http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz"
+url_ogg="http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz"
+url_theora="http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2"
+url_vorbis="http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz"
+
+gcc_ver=`gcc -dumpversion | awk -F. '{print $1}'`
+
+if [ ${gcc_ver} -ge 6 ]; then
+    # for x264, libvpx, ffmpeg
+    pic_enable="--enable-pic"
+    enable_option=${pic_enable}
+fi
+
+print_error()
+{
+    echo "error: $1"
+}
+
+run_git()
+{
+    repo="$1"
+    opt="$2"
+
+    dir=${repo##*/}
+    dir=${dir%.git}
+
+    if [ -d $dir ]; then
+        cd $dir && git pull
+        if [ $? -ne 0 ]; then
+            print_error "git pull $dir" && exit 1
+        fi
+    else
+        git clone $opt $repo
+        if [ $? -ne 0 ]; then
+            print_error "git clone $dir" && exit 1
+        fi
+        cd $dir
+    fi
+}
+
+run_wget()
+{
+    url="$1"
+    file=${url##*/}
+    dir=${file%.tar.*}
+
+    if [ ! -e $file ]; then
+        wget $url
+        if [ $? -ne 0 ]; then
+            print_error "wget $file" && exit 1
+        fi
+    fi
+
+    case $file in
+        *.gz)  tar xvzf $file ;;
+        *.bz2) tar xvjf $file ;;
+    esac
+
+    cd $dir
+}
+
+uid=`id | sed 's/uid=\([0-9]\+\)(.\+/\1/'`
+
+if [ $uid -ne 0 ];then
+    print_error "not root user"
+    exit 1
+fi
+
+mkdir -p $src_dir
+mkdir -p $prefix_dir
+
+aconf_ver=`LANG=C autoconf -V | head -n 1 | sed -e "s/autoconf (GNU Autoconf) \([0-9]*\)\.\([0-9]*\)/\1\2/"`
+if [ $aconf_ver -lt 269 ]; then
+    echo "---------- build autoconf ----------"
+    run_wget $url_autoconf
+    ./configure --prefix="$prefix_dir" --bindir="$prefix_dir/bin"
+    make
+    make install
+    make distclean
+fi
---
+++ chinachu/recCheck.js
@@ -0,0 +1,83 @@
+var fs = require('fs');
+var child_process = require('child_process').execSync;
+
+
+//ログファイルに録画した番組情報を出力
+console.error("-----------------番組情報-------------------\n");
+console.error(process.argv[3] + '\n');
+console.error("--------------------------------------------\n");
+
+//引数チェック
+//if (process.argv.length <4) return;
+
+//エンコ済みファイル倉庫（最後に"/"必須(※)
+var encodedDir = "/mnt/md127/Recs/";
+
+//録画した番組のオブジェクトを得る
+var finishedObj = JSON.parse(process.argv[3]);
+
+//録画履歴のオブジェクトを得る
+var recordedObj = JSON.parse(process.argv[2]);
+
+//過去の録画情報と今回の録画情報を比較する
+for(var i in recordedObj) {
+
+	//タイトルと詳細が一致した場合
+	if (recordedObj.title == finishedObj.title &&
+		recordedObj.detail == finishedObj.detail){
+
+		//何もせず終了
+		console.error("this movie is already encoded.\nNothing to do.");
+
+		//元ファイルを削除して終了
+//		fs.unlinkSync(finishedObj.recorded);
+//		console.error("this movie is already encoded.\nDelete source file.");
+
+		return;
+	}
+}
+
+//------------------------
+//エンコる
+//------------------------
+var recOpt = "-vcodec libx265 -b:v 16384k";
+
+//var ffmpeg = __dirname + "/usr/bin/ffmpeg";				//←通常の優先度でエンコする
+var ffmpeg = "nice -n 15 " + __dirname + "/usr/bin/ffmpeg"; //←システム全体の負荷を考慮し、エンコの優先度を落とす
+
+//ffmpegの入力ファイルオプション
+var optIn = ' -i ' + '"' + finishedObj.recorded + '"';
+
+//出力ファイルの拡張子(先頭に"."必須)
+var outExt = ".mp4";
+
+//"/mnt/HDD2/ts/[151111-1355][GR25][PT2-T1]情報ライブ　ミヤネ屋.ts",
+// ↓ こんなんにする
+//”[151111-1355][GR25][PT2-T1]情報ライブ　ミヤネ屋"
+var slashPos = finishedObj.recorded.lastIndexOf("/");
+var tmpOutFile = finishedObj.recorded.slice(slashPos+1,finishedObj.recorded.length - 3);
+
+//”[151111-1355][GR25][PT2-T1]情報ライブ　ミヤネ屋"
+// ↓ こんなんにする
+//”エンコ済みファイル倉庫パス/[151111-1355][GR25][PT2-T1]情報ライブ　ミヤネ屋.mp4"
+var outFile = " " + '"' + encodedDir + tmpOutFile + outExt + '"';
+
+//コーデック指定オプション(先頭に" "(空白)必須)
+var codecOpt = " -vcodec libx264";
+
+//エンコ実行文字列
+var execString = ffmpeg + optIn + codecOpt + recOpt + outFile;
+
+console.error("------------ffmpeg execute string-----------\n");
+console.error(execString + '\n');
+console.error("--------------------------------------------\n");
+
+//ffmpeg起動
+child_process(execString, function(err, stdout, stderr){
+
+	//子プロセスでエラー時に記録
+	var logText = "Error:" + err + "\n" + "stdout:" + stdout + "\n" + "stderr:" + stderr + "\n";
+	console.error("--------------ffmpeg error------------------\n");
+    console.error(logText + '\n');
+	console.error("--------------------------------------------\n");
+});
